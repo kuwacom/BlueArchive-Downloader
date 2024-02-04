382a4f69a16430fea30eea511589a5db22272bc8
fiseleo <123489414+fiseleo@users.noreply.github.com>
2024-02-05 01:06:20 +0800

    Now TableSerivece

--- lib/TableService.py
+++ lib/TableService.py
@@ -1,18 +1,17 @@
 # from _typeshed import StrPath
 from io import BytesIO
-from typing import IO
 from zipfile import ZipFile
 import os
 from .XXHashService import CalculateHash
+from base64 import b64encode
+from .MersenneTwister import MersenneTwister  # 替换为实际的模块导入语句
 from typing import Union
-
 
 class TableZipFile(ZipFile):
     def __init__(self, file: Union[str, BytesIO], name: str = None) -> None:
         super().__init__(file)
-        self.password = str(
-            CalculateHash(name if not isinstance(file, str) else os.path.basename(file))
-        ).encode()
+        mersenne_twister_instance = MersenneTwister(CalculateHash(name if not isinstance(file, str) else os.path.basename(file)))
+        self.password = b64encode(mersenne_twister_instance.NextBytes(15))
 
     def open(self, name: str, mode: str = "r", force_zip64=False):
         return super(self.__class__, self).open(
